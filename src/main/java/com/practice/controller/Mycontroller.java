485ce79520cc33e4b306af91ba660d8f777bdbaf
Nitya <22bch036@nith.ac.in>
2025-05-21 19:09:20 +0530

    commands and syntax of thymeleaf

--- src/main/java/com/practice/controller/Mycontroller.java
+++ src/main/java/com/practice/controller/Mycontroller.java
@@ -1,6 +1,9 @@
 package com.practice.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,9 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class Mycontroller {
 	
 	@RequestMapping(value="/about", method = RequestMethod.GET)
-	public String about() {
+	public String about(Model model) {
 		
 		System.out.println("inside about handler");
+		
+		//putting data in model
+		model.addAttribute("name", "nitya");
+		model.addAttribute("currentDate", new Date().toLocaleString());
+		
 		return "about";
 	}
 
